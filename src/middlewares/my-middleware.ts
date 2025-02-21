c711e71a36e4ecaa3acef141c2c3a278b0d0dd95
Milena Strassburger <milenastrass@gmail.com>
2025-02-21 19:15:31 -0300

    learning about global and local middleware

---
+++ src/middlewares/my-middleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+//next serve para o middleware não morrer no meio do caminho
+//a não ser que seja do seu desejo
+export function myMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+    console.log("Passou pelo Middleware!")
+
+    return next()
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import { myMiddleware } from "./middlewares/my-middleware.js"
 
 const PORT = 3333
 
 const app = express()
 app.use(express.json()) //definindo o padrão das requests para json no express
+
+//app.use(myMiddleware) //vai passar pelo middleware para fazer a requisição - porém essa forma é para usar o middleware global, para todas as rotas existentes
 
 // Parâmetros nomeados ficam dessa forma no node.js com express
 // app.get("/products/:id/:user", (request, response) => {
@@ -17,11 +20,12 @@ app.use(express.json()) //definindo o padrão das requests para json no express
         response.send(`Produtos`)
     })
 
-    app.post("/products", (request, reponse) => {
+    //caso eu queria usar um middleware em uma rota específica eu adiciono dps da rota especificada
+    app.post("/products", myMiddleware, (request, response) => {
         const {name, price} = request.body
 
         //reponse.send(`Produto ${name} custa $ ${price}`)
-        reponse.status(201).json({name, price}) //devolver resposta em formato json sem a necessidade de converter com stringfy e tb colocar o status code para retornar na mesma linha
+        response.status(201).json({name, price}) //devolver resposta em formato json sem a necessidade de converter com stringfy e tb colocar o status code para retornar na mesma linha
     })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
