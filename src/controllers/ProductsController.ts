9a1bace2318757fc33fe8e1ea85213b5fc20f2ea
Milena Strassburger <milenastrass@gmail.com>
2025-02-22 11:36:18 -0300

    handling errors

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { AppError } from "../utils/AppError.js"
 
 class ProductController {
     /**
@@ -18,6 +19,8 @@ class ProductController {
 
     create(request: Request, response: Response) {
         const {name, price} = request.body
+
+        //throw new AppError("Erro ao tentar criar um produto!")
 
         response.status(201).json({name, price, user_id: request.user_id})
     }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 import { routes } from "./routes/index.js"
+import { AppError } from "./utils/AppError.js"
 
 const PORT = 3333
 
@@ -7,5 +8,14 @@ const app = express()
 app.use(express.json()) //definindo o padrão das requests para json no express
 
 app.use(routes)
+
+app.use((error: any, request: Request, response: Response, next: NextFunction) => {
+
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({message: error.message})
+    }
+
+    response.status(500).json({ message: error.message })
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
